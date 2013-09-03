7dd43d5e81139c1cda8180055995cf8048b6a4d6
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-03 10:24:06 -0700

    finish user db table + migrate

---
+++ db/migrate/20130903171009_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+
+  def change
+    create_table 	:users do |t|
+
+      t.string 	:name
+      t.integer 	:age
+      t.string 	:location
+      t.string 	:salt   
+      t.string 	:fish
+      t.string 	:code
+      t.timestamp :expires_at
+
+      t.timestamps
+
+    end
+  end
+end
