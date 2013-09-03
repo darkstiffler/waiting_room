b252ff9efe6fc0f6990ff04eeabd814c1fba7c23
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-03 10:35:13 -0700

    add new routes + login + controllers

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 WaitingRoom::Application.routes.draw do
+
+  get "user/index"
+  get "user/show"
+  get "user/new"
+  get "user/create"
+  get "user/update"
+  get "user/edit"
+  get "user/delete"
+  get "/login" => 'site#login'
+
+  root 'site#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
