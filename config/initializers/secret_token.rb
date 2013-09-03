4ac4a4e2cea6620374f1a404b6b8f56d95cb383a
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-03 09:48:58 -0700

    add rails 4 w/ postgresql + config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WaitingRoom::Application.config.secret_key_base = 'cca891131294d727802bed44084b72566fa136aece53cfa070065691cc75c7a7439648d8218700612381d6a00757bf32f129bd26c61a01a0955dfd9a605a0c3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WaitingRoom::Application.config.session_store :cookie_store, key: '_waiting_room_session'
