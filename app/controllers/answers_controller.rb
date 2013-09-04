4900052596175e0dfeebbee6ee3c09527aba01b3
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-04 12:28:18 -0700

    add logic / routing

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,20 @@
 class AnswersController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	def edit 
+	end
+
+	def destroy
+	end
+	
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,6 +14,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def update
