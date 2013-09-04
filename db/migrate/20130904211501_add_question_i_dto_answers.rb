1a5fd048163230c4f3cb0731b702d26ce37c7d7d
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-04 14:32:32 -0700

    more on responding

---
+++ db/migrate/20130904211501_add_question_i_dto_answers.rb
@@ -0,0 +1,5 @@
+class AddQuestionIDtoAnswers < ActiveRecord::Migration
+  def change
+  	add_column(:answers, :question_id, :integer)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130904190701) do
+ActiveRecord::Schema.define(version: 20130904211501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20130904190701) do
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "question_id"
   end
 
   create_table "doctors", force: true do |t|
