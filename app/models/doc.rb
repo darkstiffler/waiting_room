447bc329c4c38284278430444a9dbf0afa3620ea
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-06 10:36:55 -0700

    clean up

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -3,6 +3,8 @@ class Doc < ActiveRecord::Base
 
 	validates :licence, numericality: { only_integer: true }
 	validates :licence,length: { maximum: 10 }
+	validates :name, :licence, uniqueness: true
+
 
 	validates :name, :licence, :password_digest, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 
 	validates :name, :age, presence: true
 	validates :name, length: {maximum: 20, minimum: 5}	
-	
+	validates :name, uniqueness: true
 end
