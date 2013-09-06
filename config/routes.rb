e8ee1f933e086f8334c44b77c1473f6b228f43bd
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-05 23:38:23 -0700

    route mod, spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 WaitingRoom::Application.routes.draw do
   root 'site#welcome'
+
+  get 'signup', to: 'docs#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :docs
   resources :doctors
   resources :users
   resources :sessions
   resources :questions
   resources :answers
+
   get "/welcome" => 'site#welcome'
 
 end
