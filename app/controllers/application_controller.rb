cd8451a8664e7ef30ba4bf95a0d4c191dd9330f8
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-05 17:37:43 -0700

    rm untracked, add css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+	def current_doc		
+	  @current_doc ||= Doc.find(session[:doc_id]) if session[:doc_id]
+	end
+	helper_method :current_doc
+
+	def authorize
+	  redirect_to login_url, alert: "Not authorized" if current_doc.nil?
+	end
 end
